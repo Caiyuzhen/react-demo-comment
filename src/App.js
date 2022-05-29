43ee37ebfd1263151a5cbc91bf30708dd800b737
Zen <47984714+Caiyuzhen@users.noreply.github.com>
2022-05-30 02:04:40 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,83 +1,138 @@
 import './index.css'
+import React from 'react'
 import avatar from './images/avatar.png'
 import avatar1 from './images/avatar02.png'
 import avatar2 from './images/avatar03.png'
 
 
+//🔥🔥渲染 DOM 数据
+//第一步，渲染评论数据，思路：用 state.xxx.map(item=><html></html>) 来获取列表数据
+//第二步，渲染 tab 、评论列表和点赞、踩的状态，思路：用三元表达式来判断【动态类名】
 
-//第一步，渲染评论数据
 
-//思路：用 state.xxx.map(item=><html></html>) 来获取列表数据
+//🔥🔥绑定点击事件去改变 tab 的 active 状态
 
-//第二步，渲染 tab 、评论列表和点赞、踩的状态
-//思路：用三元表达式来判断【动态类名】
 
-// 依赖的数据
-const state = {
-  // hot: 热度排序  time: 时间排序
-  tabs: [
-    {
-      id: 1,
-      name: '热度',
-      type: 'hot' //用来储存 tab 激活状态
-    },
-    {
-      id: 2,
-      name: '时间',
-      type: 'off'//用来储存 tab 激活状态
-    }
-  ],
-  active: 'hot',//表示 tab 激活状态,如果匹配到此状态则激活 tab
-  list: [
-    {
-      id: 1,
-      author: '小刘',
-      comment: '这个方案非常哈好',
+//🔥🔥利用受控制组件，绑定评论的数据到 state 中
 
-      time: new Date('2021-10-10 09:09:00'), //new 一个时间对象
-      avatar: avatar,
-
-      attitude: 1 // 🚀🚀 用来记录点赞的状态，1: 点赞 0：无态度 -1:踩
-    },
-    {
-      id: 2,
-      author: '小李',
-      comment: '哎哟，不错哦',
-      time: new Date('2021-10-11 09:09:00'),
-      avatar: avatar1,
-
-      attitude: 0// 🚀🚀 用来记录点赞的状态，1: 点赞 0：无态度 -1:踩
-    },
-    {
-      id: 3,
-      author: '小张',
-      comment: '不打扰你了谢谢',
-      time: new Date('2021-10-11 10:09:00'),
-      avatar: avatar2,
-
-      attitude: -1// 🚀🚀 用来记录点赞的状态，1: 点赞 0：无态度 -1:踩
-    }
-  ],
-}
+//🔥🔥点击【发表评论】的 button，拿到 state 中的评论数据并添加到 state 中去（本质是往 state.list 中添加数据）
 
 
 
 
-// 处理时间对象
+//———————————————————————————————————————————————————————————————————————————————————————
+
+
+
+
+// 处理【时间】这个事件对象
 function formatTime (time) {
 
-  //时间格式化为 2022-05-27 的时间格式
-  return ` ${time.getFullYear()} - ${time.getMonth() + 1} - ${time.getDate()} `
+  //把时间格式化为 2022-05-27 的时间格式
+  return( ` ${time.getFullYear()} - ${time.getMonth() + 1} - ${time.getDate()} `)
 }
 
 
 
 
+class App extends React.Component {
+  // 依赖的数据
+  state = {
+    // hot: 热度排序  time: 时间排序
+    tabs: [
+            {
+              id: 1,
+              name: '热度',
+              type: 'hot' //用来储存 tab 激活状态
+            },
+            {
+              id: 2,
+              name: '时间',
+              type: 'off'//用来储存 tab 激活状态
+            }
+    ],
 
-//渲染层，获取数据
-function App () {
-  return (
-    <div className="App">
+    active: 'hot',//🚀表示 【tab 激活状态】,如果匹配到此状态则激活 tab
+
+    list: [
+            {
+              id: 1,
+              author: '小刘',
+              comment: '这个方案非常哈好',
+              time: new Date('2021-10-10 09:09:00'), //new 一个时间对象
+              avatar: avatar,
+              attitude: 1 // 🚀🚀 用来记录点赞的状态，1: 点赞 0：无态度 -1:踩
+            },
+            {
+              id: 2,
+              author: '小李',
+              comment: '哎哟，不错哦',
+              time: new Date('2021-10-11 09:09:00'),
+              avatar: avatar1,
+              attitude: 0// 🚀🚀 用来记录点赞的状态，1: 点赞 0：无态度 -1:踩
+            },
+            {
+              id: 3,
+              author: '小张',
+              comment: '不打扰你了谢谢',
+              time: new Date('2021-10-11 10:09:00'),
+              avatar: avatar2,
+              attitude: -1// 🚀🚀 用来记录点赞的状态，1: 点赞 0：无态度 -1:踩
+            },
+          ],
+    comment: '', 
+  };
+
+
+  //切 tab
+  SwitchTab = (type) => { //🔥🔥传入下面 onClick 回调的参数！！
+    console.log("点击了 tab");
+    console.log("切换为",type)
+    //点击谁，就把 type 属性赋值给 state 中的 active, 所以需要传入参数
+    this.setState({
+      active: type  //把【active 的值】传给 【点击的这个 tab 的 type】！！【type】 表示 tab 的状态值！！
+    })
+  }
+
+  //获取输入框内的值
+  textAreaChange = (e) => {
+    this.setState({
+      comment: e.target.value //把【数据存储到 state 中】
+    })
+  }
+
+  // //写错了🍎，得重新看看。聚焦后清除评论框内的 placeholder
+  // focusFn = () => {
+  //   console.log(this.target.attr.placeholder)
+  // }
+
+
+  //提交评论(本质是往 state.list 中添加数据)
+  submitFn = () => {
+    this.setState({
+      list:[
+        ...this.state.list, //拿到原来 state 中的数据 （人+名字+评论+时间）
+        {
+          id: 1, //自己生成独一无二的值
+          author: '小王',
+          comment: 'this.state.comment', //🔥🔥这个应该换成 state 中的数据
+          time: new Date(), //new 一个时间对象
+          avatar: avatar,
+          attitude: 0 // 🚀🚀 用来记录点赞的状态，1: 点赞 0：无态度 -1:踩
+        }
+      ]
+    })
+  }
+
+
+  
+
+
+
+  //渲染层，获取数据
+  render() {
+    return (
+      <div className="App">
       <div className="comment-container">
 
 
@@ -92,37 +147,53 @@ function App () {
           <ul className="sort-container">
 
 
-            {/* 🚀🚀🚀 拿到 tab 的名字, 通过 name 拿到 */}
-            {state.tabs.map(item =>
-
+            {/* 🚀🚀🚀 判断当前激活的是哪个【 Tab 】, 通过 state.name 拿到 */}
+            {this.state.tabs.map(tab =>(  //遍历 item （也就是 state ）`tab` 为 item ，可以自定义命名
 
               // 🚀🚀🚀 判断激活状态，如果匹配到此 state 的 type 为 active 的话，则给它添加一个激活的 on className，这时候就会显示高亮状态
-              <li key={item.id} className={item.type === state.active ? 'on' : ''}>   按{item.name}排序   </li>
+              <li 
+                  key={tab.id} 
+                  className={tab.type === this.state.active ? 'on' : ''}//默认状态
 
-            )}
+                  // onClick={ this.SwitchTab } //🔥🔥注意， onClick={ this.SwitchTab }  这样的写法没法透出参数，然后去绑定 active 状态！
+                  onClick={ ()=>this.SwitchTab(tab.type) } //🔥🔥注意，相比于上面，这样的写法才可以把参数回调出去！！然后去绑定 active 状态！
+
+              > 按{tab.name}排序 </li>
+            ))}
 
 
-
-            {/*  👇 用 on 来激活 li ，用上面的写法了,通过拿到的 name 来判断是哪个 tab，所以可以不要*/}
+            {/*  👇 方法二：可以用 on 来激活 li ，因为用上面已经【通过拿到的 name 的值 】来判断是哪个 tab，所以这里可以不要  */}
             {/* <li className="on"></li>
             <li>按时间排序</li> */}
 
           </ul>
         </div>
 
-        {/* 添加评论的输入框 */}
+        {/* 添加评论的输入框 ,通过受控组件的方式去拿到 Value*/}
         <div className="comment-send">
           <div className="user-face">
             <img className="user-head" src={avatar} alt="" />
           </div>
+
+          {/* 输入框 */}
           <div className="textarea-container">
             <textarea
               cols="80"
               rows="5"
-              placeholder="发条友善的评论"
+              placeholder="😄发条友善的评论"
               className="ipt-txt"
+              value={this.state.comment} //🚀绑定评论输入框的值
+              onChange={this.textAreaChange} //🚀绑定评论输入框的的函数
+              // <button onClick={ ()=> this.handler() }>点击我</button>
+              // onFocus={ this.focusFn } //聚焦后清除数据
             />
-            <button className="comment-submit">发表评论</button>
+
+            {/* 发表评论按钮 */}
+            <button 
+              className="comment-submit"
+              onClick={this.submitFn}
+            >发表评论</button>
+
           </div>
           <div className="comment-emoji">
             <i className="face"></i>
@@ -133,10 +204,10 @@ function App () {
         {/* 评论的列表 */}
         <div className="comment-list">
 
-          {/* 🌟🌟🌟 列表渲染，用 map 方法 */}
-          {state.list.map(item => (
+          {/* 🌟🌟🌟 列表渲染，用 map 方法，🔥🔥遍历 【state】里边的数据 */}
+          {this.state.list.map(item => (
 
-            // 🚀记得加 key！，也就是list item 的 id！
+            // 🚀记得加 key！，也就是【list item 】的 id！
             <div className="list-item" key={item.id}>
               <div className="user-face">
                 <img className="user-head" src={item.avatar} alt="" />
@@ -176,8 +247,10 @@ function App () {
         </div>
       </div>
     </div>
-  )
+    );
+  }
 }
+
 
 
 
